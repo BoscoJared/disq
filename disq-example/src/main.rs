328e6da0da0218c3d5ebd40817ae04bcc4338618
Jared Bosco <boscojared@gmail.com>
2022-04-16 17:26:17 -0400

    [disq][core] get initial basic implementation of writing to discord

---
+++ disq-example/src/main.rs
@@ -0,0 +1,33 @@
+use async_trait::async_trait;
+use disq::yeet::{Destination, YeetOptions, Yeeter, YeeterBuilder};
+use serenity::client::{Client, Context, EventHandler};
+use serenity::http::Http;
+use serenity::model::prelude::Ready;
+use tokio::task;
+
+const TOKEN: &str = "BOT_TOKEN";
+
+#[tokio::main]
+async fn main() {
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
+
+    let token = std::env::var(TOKEN).expect("Could not load the bot token!");
+    let builder = Client::builder(token);
+
+    let (builder, recv) = YeeterBuilder::<&str>::register(builder, Destination, YeetOptions);
+
+    let handle = tokio::spawn(async move {
+        let mut client = builder.await.expect("Could not build the client!");
+        println!("Starting up the bot...");
+        if let Err(err) = client.start().await {
+            println!("Whoops, that was an error! {:?}", err);
+        }
+    });
+
+    let yeeter = recv.recv().expect("Could not get the passed back Yeeter");
+    yeeter.yeet("From main.rs:28!").await.expect("whoops");
+
+    if let Err(err) = handle.await {
+        log::error!("Whoops: {:?}", err);
+    };
+}
